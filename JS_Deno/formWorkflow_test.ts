b9ea4511d1f1047aa414d92abeb94bac4ae9e972
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2023-10-14 19:38:54 -0400

    use assert stdlib

--- JS_Deno/formWorkflow_test.ts
+++ JS_Deno/formWorkflow_test.ts
@@ -1,3 +1,4 @@
+import { assertExists, assertEquals } from 'https://deno.land/std@0.204.0/assert/mod.ts';
 import Helper from './helper.ts';
 
 let h = new Helper();
@@ -5,20 +6,12 @@ await h.init();
 let fetch = h.wrapFetch();
 let rootURL = h.rootURL;
 
-Deno.test("api/form/version", async () => {
-    let res = await fetch(rootURL + `api/form/version`);
+Deno.test("formWorkflow: currentStep person designated AND group requirements", async () => {
+    let res = await fetch(rootURL + `api/formWorkflow/484/currentStep`);
     let val = await res.json()
 
-    if(val != 1) {
-        throw Error("Version doesn't match");
-    }
-});
+    assertEquals(val[9].description, 'Group A');
+    assertEquals(val[-1].description, 'Step 1 (Omar Marvin)');
 
-Deno.test("api/form/query - homepage", async () => {
-    let res = await fetch(rootURL + `api/form/query?q={"terms":[{"id":"title","operator":"LIKE","match":"***","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`);
-    let val = await res.json()
-
-    if(val[504].recordID != 504) {
-        throw Error("Wrong record ID");
-    }
+    assertExists(!val[9].approverName); // approverName should not exist for depID 9
 });
--- JS_Deno/form_test.ts
+++ JS_Deno/form_test.ts
@@ -1,3 +1,4 @@
+import { assertExists, assertEquals } from 'https://deno.land/std@0.204.0/assert/mod.ts';
 import Helper from './helper.ts';
 
 let h = new Helper();
@@ -5,20 +6,25 @@ await h.init();
 let fetch = h.wrapFetch();
 let rootURL = h.rootURL;
 
-Deno.test("api/form/version", async () => {
+Deno.test("form: version", async () => {
     let res = await fetch(rootURL + `api/form/version`);
     let val = await res.json()
 
-    if(val != 1) {
-        throw Error("Version doesn't match");
-    }
+    assertEquals(val, "1");
 });
 
-Deno.test("api/form/query - homepage", async () => {
+Deno.test("form: query homepage", async () => {
     let res = await fetch(rootURL + `api/form/query?q={"terms":[{"id":"title","operator":"LIKE","match":"***","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`);
     let val = await res.json()
 
-    if(val[504].recordID != 504) {
-        throw Error("Wrong record ID");
-    }
+    assertEquals(val[504].recordID, 504);
+});
+
+Deno.test("form: non-admin query for actionable records", async () => {
+    let res = await fetch(rootURL + `api/form/query?q={"terms":[{"id":"stepID","operator":"=","match":"actionable","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service"],"sort":{},"limit":1000,"limitOffset":0}&x-filterData=recordID,title&masquerade=nonAdmin`);
+    let val = await res.json()
+
+    assertExists(val[503]); // tester is backup of person designated
+    assertExists(val[504]); // tester is backup of initiator
+    assertExists(!val[505]); // tester is not the tester
 });
