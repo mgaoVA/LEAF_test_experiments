3d3e18d8680d54fa8121d6e9031c0532653d5a17
mgaoVA <16783916+mgaoVA@users.noreply.github.com>
2023-11-03 17:07:17 -0400

    update and prune test DB

--- Go/formQuery_test.go
+++ Go/formQuery_test.go
@@ -3,38 +3,41 @@ package main
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"strings"
 	"testing"
 )
 
-func getFormQuery(url string) FormQueryResponse {
+func getFormQuery(url string) (FormQueryResponse, error) {
 	url = strings.Replace(url, " ", "%20", -1)
 	res, _ := client.Get(url)
 	b, _ := io.ReadAll(res.Body)
 
 	var m FormQueryResponse
 	err := json.Unmarshal(b, &m)
-	if err != nil {
-		log.Printf("JSON parsing error, couldn't parse: %v", string(b))
-		log.Printf("JSON parsing error: %v, while retrieving %v", err.Error(), url)
-	}
-	return m
+
+	return m, err
 }
 
-func TestForm_HomepageQuery(t *testing.T) {
-	res := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"title","operator":"LIKE","match":"***","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+func TestFormQuery_HomepageQuery(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"title","operator":"LIKE","match":"***","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
 
-	got := res[460].RecordID
-	want := 460
+	// get first key
+	var key int
+	for k, _ := range res {
+		key = k
+		break
+	}
+
+	got := res[key].RecordID
+	want := key
 
 	if got != want {
 		t.Errorf("RecordID = %v, want = %v", got, want)
 	}
 }
 
-func TestForm_NonadminQueryActionable(t *testing.T) {
-	res := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"stepID","operator":"=","match":"actionable","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service"],"sort":{},"limit":1000,"limitOffset":0}&x-filterData=recordID,title&masquerade=nonAdmin`)
+func TestFormQuery_NonadminQueryActionable(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"stepID","operator":"=","match":"actionable","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service"],"sort":{},"limit":1000,"limitOffset":0}&x-filterData=recordID,title&masquerade=nonAdmin`)
 
 	if _, exists := res[503]; !exists {
 		t.Errorf("Record 503 should be readable because tester is backup of person designated")
@@ -53,28 +56,28 @@ func TestForm_NonadminQueryActionable(t *testing.T) {
 	}
 }
 
-func TestForm_FulltextSearch_ApplePearOrange(t *testing.T) {
-	res := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear orange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+func TestFormQuery_FulltextSearch_ApplePearOrange(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear orange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
 
 	if _, exists := res[499]; !exists {
-		t.Errorf(`Record 499 should be returned because the data fields contain either apple, pear, or orange`)
+		t.Errorf(`Record 499 should be returned because a data field contains either apple, pear, or orange`)
 	}
 
 	if _, exists := res[498]; !exists {
-		t.Errorf(`Record 499 should be returned because the data field contain either apple, pear, or orange`)
+		t.Errorf(`Record 498 should be returned because a data field contains either apple, pear, or orange`)
 	}
 }
 
-func TestForm_FulltextSearch_ApplePear_RequireOrange(t *testing.T) {
-	res := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear %2Borange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+func TestFormQuery_FulltextSearch_ApplePear_RequireOrange(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear %2Borange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
 
 	if _, exists := res[499]; !exists {
-		t.Errorf(`Record 499 should be returned because the data field contains the word "orange"`)
+		t.Errorf(`Record 499 should be returned because a data field contains the word "orange"`)
 	}
 }
 
-func TestForm_FulltextSearch_ApplePearNoOrange(t *testing.T) {
-	res := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear %2Dorange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
+func TestFormQuery_FulltextSearch_ApplePearNoOrange(t *testing.T) {
+	res, _ := getFormQuery(rootURL + `api/form/query?q={"terms":[{"id":"data","indicatorID":"3","operator":"MATCH","match":"apple pear %2Dorange","gate":"AND"},{"id":"deleted","operator":"=","match":0,"gate":"AND"}],"joins":["service","status","categoryName"],"sort":{"column":"date","direction":"DESC"},"limit":50}`)
 
 	if _, exists := res[499]; exists {
 		t.Errorf(`Record 499 should not be returned because the data field contains the word "orange". want = no orange`)
